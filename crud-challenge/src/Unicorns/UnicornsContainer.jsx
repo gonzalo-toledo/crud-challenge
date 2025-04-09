35f73d253b65c75073da52c6411ce3b76588bccd
Leon Federico Huck <leonhuck007@gmail.com>
2025-04-08 23:08:08 -0300

    ahora andan el update

--- crud-challenge/src/Unicorns/UnicornsContainer.jsx
+++ crud-challenge/src/Unicorns/UnicornsContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import UnicornsView from "./UnicornsView";
 import { useState, useEffect } from "react";
 
-const API_URL = 'https://crudcrud.com/api/6baa6fac841f4334aa172e1aaf025feb/unicorns';
+const API_URL = 'https://crudcrud.com/api/23e9c2b3676d42f7a2a4e5990b9d5d71/unicorns';
 
 const UnicornsContainer = () => {
     const [unicorns, setUnicorns] = useState([]);
@@ -53,22 +53,28 @@ const UnicornsContainer = () => {
     const updateUnicorn = async (id, updatedData) => {
         try {
             setLoading(true);
+            console.log(id, updatedData);
             const response = await fetch(`${API_URL}/${id}`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Accept': 'application/json'
+
                 },
-                body: JSON.stringify(updatedData),
+                body: JSON.stringify({
+                    name: updatedData.name,
+                    age: updatedData.age,
+                    color: updatedData.color,
+                    power: updatedData.power
+                }),
                 mode: 'cors' // Asegúrate de que está en modo cors
             });
     
             if (!response.ok) {
                 throw new Error(`Error ${response.status}: ${response.statusText}`);
             }
-    
-            const updatedUnicorn = await response.json();
-            setUnicorns(unicorns.map(u => u._id === id ? updatedUnicorn : u));
+            console.log(response);
+            // const updatedUnicorn = await response.json();
+            // setUnicorns(unicorns.map(u => u._id === id ? updatedUnicorn : u));
             return true;
         } catch (error) {
             setError(`Error al actualizar: ${error.message}`);
@@ -77,6 +83,7 @@ const UnicornsContainer = () => {
         } finally {
             setLoading(false);
             setEditingUnicorn(null);
+            fetchUnicorns();
         }
     };
 
